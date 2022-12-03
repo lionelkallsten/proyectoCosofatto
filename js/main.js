481ada760f28ecbc985cdd4974f8b2098131d5ad
lionelkallsten <lionelkallstenyt@gmail.com>
2022-12-03 00:02:03 -0300

    productos repetidos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let productos=[];
+let productos = [];
 
 let totalCompra;
 
@@ -13,8 +13,8 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 (carrito.length != 0) && renderizarCarritoInicial();
 getJSON();
 
-function renderizarProductos(){
-    for(const producto of productos){
+function renderizarProductos() {
+    for (const producto of productos) {
         content.innerHTML += `
         <div class="col">
             <div class="card" style="width: 18rem;">
@@ -33,7 +33,7 @@ function renderizarProductos(){
 
     //EVENTOS
     productos.forEach((producto) => {
-        document.getElementById(`btn${producto.id}`).addEventListener("click", function(){
+        document.getElementById(`btn${producto.id}`).addEventListener("click", function () {
             agregarAlCarrito(producto);
         });
     });
@@ -51,43 +51,54 @@ async function getJSON() {
 }
 
 
-function agregarAlCarrito(productoAComprar){
-    carrito.push(productoAComprar);
-    //console.table(carrito);
-    //alert("Producto "+productoAComprar.nombre+" agregado al carro!");
+function agregarAlCarrito(productoAComprar) {
+    const repetido = carrito.some((productoRepetido) => productoRepetido.id === productoAComprar.id);
+    if (repetido) {
+        carrito.map((product) => {
+            if (product.id === productoAComprar.id) {
+                product.cantidad++;
+            }
+        })
+    } else {
+        carrito.push(productoAComprar);
+    }
     modalContent.innerHTML += `
-        <tr>
-            <th scope="row"><img
-                src="${productoAComprar.img}" alt="prod" style="width: 4.5rem;"></th>
-            <td>${productoAComprar.nombre}</td>
-            <td>$ ${productoAComprar.precio}</td>
-            <td>cantidad</td>
-            <td>calcular</td>
-            <td><button type="button" class="btn btn-outline-danger" style="width: 2.2rem;">-</button><button
-                type="button" class="btn btn-outline-success" style="width: 2.2rem;">+</button></td>
-        </tr>
-    `;
-    totalCompra = carrito.reduce((acm, producto) => acm + producto.precio, 0);
+    <tr>
+        <th scope="row"><img
+            src="${productoAComprar.img}" alt="prod" style="width: 4.5rem;"></th>
+        <td>${productoAComprar.nombre}</td>
+        <td class="text-center">$ ${productoAComprar.precio}</td>
+        <td class="text-center">${productoAComprar.cantidad}</td>
+        <td class="text-center">${productoAComprar.cantidad * productoAComprar.precio}</td>
+        <td><button type="button" class="btn btn-outline-danger" style="width: 2.2rem;">-</button><button
+            type="button" class="btn btn-outline-success" style="width: 2.2rem;">+</button></td>
+    </tr>
+`;
+
+    console.table(carrito);
+    //alert("Producto "+productoAComprar.nombre+" agregado al carro!");
+
+    totalCompra = carrito.reduce((acm, producto) => acm + (producto.precio * producto.cantidad), 0);
     renderTotal.innerText = "Total a pagar $: " + totalCompra;
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-function renderizarCarritoInicial(){
-    for(const producto of carrito){
+function renderizarCarritoInicial() {
+    for (const producto of carrito) {
         modalContent.innerHTML += `
         <tr>
             <th scope="row"><img
                 src="${producto.img}" alt="prod" style="width: 4.5rem;"></th>
             <td>${producto.nombre}</td>
-            <td>$ ${producto.precio}</td>
-            <td>cantidad</td>
-            <td>calcular</td>
+            <td class="text-center">$ ${producto.precio}</td>
+            <td class="text-center">${producto.cantidad}</td>
+            <td class="text-center">${producto.cantidad * producto.precio}</td>
             <td><button type="button" class="btn btn-outline-danger" style="width: 2.2rem;">-</button><button
                 type="button" class="btn btn-outline-success" style="width: 2.2rem;">+</button></td>
         </tr>
     `;
     }
-    totalCompra = carrito.reduce((acm, producto) => acm + producto.precio, 0);
+    totalCompra = carrito.reduce((acm, producto) => acm + (producto.precio * producto.cantidad), 0);
     let renderTotal = document.getElementById("totalCompra");
     renderTotal.innerText = "Total a pagar $: " + totalCompra;
 }
@@ -95,9 +106,9 @@ function renderizarCarritoInicial(){
 //BOTON VACIAR CARRITO
 
 vaciar.onclick = () => {
-    carrito=[];
+    carrito = [];
     localStorage.removeItem("carrito");
-    modalContent.innerHTML =``;
+    modalContent.innerHTML = ``;
     renderTotal.innerHTML = ``;
 };
 
@@ -105,7 +116,7 @@ vaciar.onclick = () => {
 //BOTON FINALIZAR COMPRA
 
 terminar.onclick = () => {
-    if (carrito.length==0){
+    if (carrito.length == 0) {
         //alerta de carrito vacio
     }
     else {
@@ -126,5 +137,5 @@ finalizarCompra()
 //agregar botones de agregar o  quitar unidades (juntar repetidos)
 //local storage
 
-//Busco cambiar el diseño del carriito modal, de cartas a tablas.Ademas, agregar los botones de agregar, quitar y eliminar productos. Debo agregar lo visto en la clase de Storage. Mucho muy importante agregar en la tabla los precios totales y parciales. 
+//Busco cambiar el diseño del carriito modal, de cartas a tablas.Ademas, agregar los botones de agregar, quitar y eliminar productos. Debo agregar lo visto en la clase de Storage. Mucho muy importante agregar en la tabla los precios totales y parciales.
 //Estoy tapado de laburo y parciales de la universidad. En la semana lo termino, perdón.
